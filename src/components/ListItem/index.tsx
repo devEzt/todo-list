e5d8f06e88b20649aa8e65e2b9e70735c4948371
Alexandre Maciel <alexandre.maciel@ciss.com.br>
2022-07-14 23:43:56 -0300

    Implementa melhoria de lógica atrelada ao valor da variavel sendo necessária ser alterada para se obter o valor necessário.

--- src/components/ListItem/index.tsx
+++ src/components/ListItem/index.tsx
@@ -5,13 +5,13 @@ import { Item } from '../../types/Item'
 
 type PropsItem = {
   item: Item
+  onChange: (id: number, done: boolean) => void
 }
 
-export const ListItem = ({ item }: PropsItem) => {
-  const [isChecked, setIsChecked] = useState(item.done)
+export const ListItem = ({ item, onChange }: PropsItem) => {
   return (
-    <C.Container done={isChecked}>
-      <input type="checkbox" checked={isChecked} onChange={(e) => setIsChecked(e.target.checked)} />
+    <C.Container done={item.done}>
+      <input type="checkbox" checked={item.done} onChange={(e) => onChange(item.id, e.target.checked)} />
 
       <label>{item.name}</label>
     </C.Container>
